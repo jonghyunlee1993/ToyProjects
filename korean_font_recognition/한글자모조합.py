aa1bb71a8723f44a3f46367e9e57a5297c9e0950
Jonghyun Lee <green198800@yonsei.ac.kr>
2020-04-21 22:48:30 +0900

    한글 자모 조합 추가

---
+++ korean_font_recognition/한글자모조합.py
@@ -0,0 +1,32 @@
+
+import pandas as pd
+from unicode import *
+
+# 초성 리스트. 00 ~ 18
+CHOSUNG_LIST = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+
+# 중성 리스트. 00 ~ 20
+JUNGSUNG_LIST = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
+
+# 종성 리스트. 00 ~ 27 + 1(1개 없음)
+JONGSUNG_LIST = [' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+
+korean_char_list = []
+
+for chosung in CHOSUNG_LIST:
+	for jungsung in JUNGSUNG_LIST:
+		for jongsung in JONGSUNG_LIST:
+			korean_character = join_jamos(chosung + jungsung + jongsung)
+			korean_char_list.append(korean_character)
+
+with open('korean_charaters.txt', 'w') as f:
+    for item in korean_char_list:
+        f.write("%s\t" % item)
+
+			
+
+
+
+
+
+
